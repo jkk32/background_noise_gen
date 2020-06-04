50d009d115f644f13a2dcddebc6b8eeff07390a5
Jussi K. Kuusisto <jkk32@sussex.ac.uk>
2020-06-04 17:00:32 +0100

    Adding the scripts.

---
+++ background_example.py
@@ -0,0 +1,81 @@
+import make_background
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+pixel_scale = 0.13              # arcsec/pixel
+aperture_radius = 10*0.175         # aperture radius in arcsec
+zeropoint = 25.946              # AB mag zeropoint
+aperture_f_limit = 4.365        # aperture flux limit (nJy)
+
+r = aperture_radius/pixel_scale # aperture radius in pixels
+
+
+# Initialise background(s)
+background_object = make_background.Background(zeropoint=zeropoint, pixel_scale=pixel_scale, aperture_f_limit=aperture_f_limit, aperture_significance=5, aperture_radius=aperture_radius, verbose = True)
+
+
+# create background image object (cutoutwidth in pixels)
+cutoutwidth = 1001
+img = background_object.create_background_image(cutoutwidth)
+
+
+'''img.bkg gives the background (random normally distributed) noise'''
+
+sig = (img.bkg/img.noise) # gives the significance image (used here just for plotting)
+
+
+plt.figure()
+plt.imshow(sig, cmap='Greys', vmin=-5.0, vmax=5.0, origin='lower')
+plt.xlabel('x / pix')
+plt.ylabel('y / pix')
+#plt.show()
+
+
+# --------------------------
+
+# Calculating flux in random apertures as check.
+
+from photutils import aperture_photometry
+from photutils import CircularAperture
+
+
+N = 1000       # Number of apertures
+
+# Random aperture positions
+positions = [np.random.randint(0+int(np.ceil(r)), cutoutwidth-int(np.ceil(r)), N), np.random.randint(0+int(np.ceil(r)), cutoutwidth-int(np.ceil(r)), N)]
+
+# Creating apertures
+aperture = CircularAperture(positions, r=r)
+
+aperture.plot(color='red', lw=1., alpha=0.5)
+
+plt.show()
+
+
+# Measuring photometry in the random apertures
+phot_table = aperture_photometry(img.bkg/img.nJy_to_es, aperture, method='subpixel')
+phot_table['aperture_sum'].info.format = '%.8g'
+
+print('')
+print('Printout of random aperture values (nJy)')
+
+#print(phot_table)
+
+aperture_sums = np.asarray(phot_table['aperture_sum'])
+
+print(f'mean aperture flux = {np.mean(aperture_sums):.2f}, std = {np.std(aperture_sums):.2f}')
+
+
+# plotting a normalised histogram of the aperture fluxes
+
+import scipy.stats
+
+x = np.linspace(-5, 5, 1000)
+plt.hist(aperture_sums, bins=100, normed=True, range=[-5, 5])
+plt.plot(x, scipy.stats.norm.pdf(x, 0, (aperture_f_limit/5)), 'r--')
+plt.axvline(0, color='black')
+plt.ylabel('PDF')
+plt.xlabel('aperture flux (nJy)')
+plt.show()
---
+++ make_background.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class empty: pass
+
+
+class Background():
+    def __init__(self, zeropoint, pixel_scale, aperture_f_limit, aperture_significance, aperture_radius, verbose = False):
+
+        '''
+        :param zeropoint:               Observation instrument/filter AB magnitude zeropoint
+        :param pixel_scale:             Pixel scale (arcsec/pixel)
+        :param aperture_f_limit:        Aperture flux limit (nJy) (The flux detected at aperture_significance)
+        :param aperture_significance:   Aperture significance (S/N)
+        :param aperture_radius:         Aperture radius (in arcsec)
+        :param verbose:                 Verbose mode toggle
+        '''
+
+
+
+        self.pixel_scale = pixel_scale
+        self.zeropoint = zeropoint # AB magnitude zeropoint # AB magnitude zeropoint
+        self.nJy_to_es = 1E-9 * 10**(0.4*(self.zeropoint-8.9)) # conversion from nJy to e/s
+
+        self.aperture = empty()
+        self.aperture.flux_limit = aperture_f_limit
+        self.aperture.radius = aperture_radius / self.pixel_scale      # aperture radius in pixels
+        self.aperture.significance = aperture_significance
+        self.aperture.noise = self.aperture.flux_limit/self.aperture.significance # nJy
+        self.aperture.background = self.aperture.noise**2
+        self.aperture.area = np.pi * self.aperture.radius**2
+        self.aperture.noise_es = self.aperture.noise * self.nJy_to_es # convert from nJy to e/s
+
+        self.pixel = empty()
+        self.pixel.background = self.aperture.background/self.aperture.area
+        self.pixel.noise = np.sqrt(self.pixel.background) # nJy
+        self.pixel.noise_es = self.pixel.noise * self.nJy_to_es # convert from nJy to e/s
+
+        if verbose:
+            print('assumed aperture radius: {0:.2f} pix'.format(self.aperture.radius))
+            print('noise in aperture: {0:.2f} nJy, {1:.4f} e/s'.format(self.aperture.noise, self.aperture.noise_es))
+            print('noise in pixel: {0:.2f} nJy, {1:.4f} e/s'.format(self.pixel.noise, self.pixel.noise_es))
+            print('zeropoint: {0}'.format(self.zeropoint))
+            print('nJy_to_es: {0}'.format(self.nJy_to_es))
+
+
+    def create_background_image(self, CutoutWidth):
+        '''
+        :param CutoutWidth:             Cutout image size in pixels
+
+        returns:                        image object: img.bkg gives the background noise map
+        '''
+
+        img = empty()
+        img.nJy_to_es = self.nJy_to_es
+        img.pixel_scale = self.pixel_scale
+        img.noise = self.pixel.noise_es * np.ones((CutoutWidth,CutoutWidth))
+        img.wht = 1./img.noise**2
+        img.bkg = self.pixel.noise_es * np.random.randn(*img.noise.shape)
+        return img
